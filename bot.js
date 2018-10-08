e021f1f9fdc9c714a0d022a9d2ce114467f6f7a5
kareem0018 <43916098+kareem0018@users.noreply.github.com>
2018-10-08 17:33:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -841,34 +841,6 @@ client.on('message', msg => { // Leaked by [ @Fr3on Gamer#9338 ]
 }
 });
 
-  client.on('message', message => { // Leaked by [ @Fr3on Gamer#9338 ]
-  if (message.author.codes) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  var command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-var args = message.content.split(" ").slice(1);
-
-  if (command == "ban") {
-               if(!message.channel.guild) return message.reply('** This command only for servers**');
-
-  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**انت لا تملك الصلاحيات المطلوبه**");
-  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
-  let user = message.mentions.users.first();
-
-  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
-  if (!message.guild.member(user)
-  .bannable) return message.reply("**يجب ان تكون رتبة البوت اعلي من رتبه الشخص المراد تبنيدة**");
-
-
-  message.guild.member(user).ban(7, user);
-
-message.channel.send(`**✅ ${user.tag} banned from the server ! ✈ **  `)
-
-}
-});
-
 
 //روحو سطر 26 الحييين
 
