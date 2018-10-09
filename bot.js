a39e653a7f3722c4afca35d772e42740cba557ed
kareem0018 <43916098+kareem0018@users.noreply.github.com>
2018-10-09 22:36:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,6 +35,24 @@ client.on('ready', () => {
 });
 
 ////////////////////////////////////
+const adminprefix = "^";
+    client.on('message', message => {
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
+      
+        let command = message.content.split(" ")[0];
+        command = command.slice(prefix.length);
+      
+      
+      let args = message.content.split(" ").slice(1);
+      let x = args.join(" ")
+        if(message.content.startsWith(adminprefix + 'say')) {
+            message.channel.send(''+x);
+                message.delete(999)
+        }
+        
+       
+      });
 ////////////////////////////////////
 client.on('message', message => {
     if (!message.content.startsWith(prefix)) return;
