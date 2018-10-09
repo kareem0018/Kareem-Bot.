39ce8f87e25ec3d8905a22cfd29d8f6796515d5c
kareem0018 <43916098+kareem0018@users.noreply.github.com>
2018-10-09 13:49:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,27 @@ client.on('ready', () => {
 
 });
 
-/////////////////////////////////////
+////////////////////////////////////
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+if (command == "!say") {
+if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return message.reply("*لا تملك الصلاحيات المطلوبه**");
+
+message.channel.send(args.join("  "))
+    message.delete();
+  }
+
+
+
+});
+////////////////////////////////////
 client.on('message', message => {
   if (message.author.x5bz) return;
   if (!message.content.startsWith(prefix)) return;
